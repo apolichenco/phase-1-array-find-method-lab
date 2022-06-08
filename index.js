e682260d820a5e1492ac819e55df4cba253b38b0
abraham <apolichenco@gmail.com>
2022-06-08 15:01:58 -0400

    commit tree 6d714a57b3851099520bcb76438e36e919873bef

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(array){
+    let results = array.find(winFinder);
+  console.log(results)
+    if (typeof results === `undefined`){
+        return results
+    }
+    else{
+        return results.year
+    }
+   }
+   function winFinder(element){
+     return (element.result === `W`);
+   }
